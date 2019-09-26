5a202e459f65ea5373f68f9182f1c18fa565184f
Melvin <MelvinTo@gmail.com>
2019-09-26 11:58:50 +0800

    Update test_flowgraph.sh

--- tests/test_flowgraph.sh
+++ tests/test_flowgraph.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-touch /tmp/hatu_test_flowgraph
+COUNT=$(redis-cli keys 'flowgraph:*' | xargs -n 1 -I ZZZ redis-cli ttl ZZZ | fgrep -- '-1' | wc -l)
+EID=$(redis-cli hget sys:ept eid)
+curl https://diag.firewalla.com/setup/flow/${EID}/${COUNT}

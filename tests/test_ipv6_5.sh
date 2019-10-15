50a0332a6457149c64e0c451c2370bb55e423deb
Melvin <MelvinTo@gmail.com>
2019-10-15 23:33:37 +0800

    Create test_ipv6_5.sh

---
+++ tests/test_ipv6_5.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+OS=$(uname -m)
+IPV6_FLAG=$(redis-cli hget sys:features ipv6)
+EID=$(redis-cli hget sys:ept eid)
+
+# Only focus on boxes with ipv6 enabled
+if [[ "1" != $IPV6_FLAG ]]; then
+	exit 0
+fi
+
+sudo systemctl restart bitbridge6

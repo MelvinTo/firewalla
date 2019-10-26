472443323ec941ff9aec8bd4835c9f66b50890f6
Melvin <MelvinTo@gmail.com>
2019-10-26 20:45:17 +0800

    Create test_bro.sh

---
+++ tests/test_bro.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [[ ! -e /blog/current/conn.log ]]; then
+  exit 0
+fi
+
+LAST_CONN_TS=$(tail -n 1 /blog/current/conn.log | json_pp | grep -w ts | awk '{print $3}' | sed 's=\..*$==')
+
+NOW=$(date +%s)
+DIFF=$(( $NOW - $LAST_CONN_TS ))
+
+EID=$(redis-cli hget sys:ept eid)
+
+if [[ $DIFF -gt 3600 ]]; then
+  curl https://diag.firewalla.com/setup/bro/${EID}/$DIFF
+fi

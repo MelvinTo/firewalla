b87d83c5c873dbe86c58734183ddc33a617bbbfe
Melvin <MelvinTo@gmail.com>
2019-11-06 16:17:00 +0800

    Update test_dmesg.sh

--- tests/test_dmesg.sh
+++ tests/test_dmesg.sh
@@ -2,4 +2,6 @@
 
 EID=$(redis-cli hget sys:ept eid)
 COUNT=$(dmesg | egrep 'dwmac-sun8i 1c30000.ethernet eth0: Link is Down' | wc -l)
-curl https://diag.firewalla.com/setup/dmesg/${EID}/${COUNT}
+UPTIME=$(echo $(awk '{print $1}' /proc/uptime) / 3600 | bc)
+RATIO=$(( $COUNT * 24 * 7 / $UPTIME ))
+curl https://diag.firewalla.com/setup/dmesg/${EID}/${COUNT}/${UPTIME}/${RATIO}

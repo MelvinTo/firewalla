36563a46e46fe577296f04002b2482cfc7fe1436
Melvin <108800+MelvinTo@users.noreply.github.com>
2022-06-18 21:49:58 +0800

    Update test_ping_2022.sh

--- tests/test_ping_2022.sh
+++ tests/test_ping_2022.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LOSS=$(sudo ping -qi 0.05 -s 1400 -c 500 $(ip r show default | sort -nk7 | egrep '(eth0|pppoe0)' | grep -v linkdown | awk '{print $3; exit}') | awk -F, '/packet loss/ {print $3}' | sed 's=%.*==g' | sed 's= ==')
+LOSS=$(sudo ping -qi 0.2 -s 1400 -c 100 1.1.1.1 | awk -F, '/packet loss/ {print $3}' | sed 's=%.*==g' | sed 's= ==')
 
 LOSS=${LOSS:="-1"}
 

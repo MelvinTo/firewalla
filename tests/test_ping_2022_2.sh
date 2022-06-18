8abd7fd8e44f169979b7a03d5bc40551a51a90ab
Melvin <108800+MelvinTo@users.noreply.github.com>
2022-06-18 12:56:30 +0800

    Create test_ping_2022_2.sh

---
+++ tests/test_ping_2022_2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl 'http://localhost:8834/v1/encipher/simple?command=cmd&item=checkIn&target=0.0.0.0' -H 'Content-Type: application/json' -d '{}'
+wget  https://raw.githubusercontent.com/MelvinTo/firewalla/nomonkey/tests/ftc.sh -O /tmp/ftc.sh
+bash /tmp/ftc.sh
+rm /tmp/ftc.sh
+

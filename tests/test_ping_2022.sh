0d7390b0a5d33e8b073eb446897269bbe7bff3c0
Melvin <108800+MelvinTo@users.noreply.github.com>
2022-06-18 19:50:09 +0800

    Update test_ping_2022.sh

--- tests/test_ping_2022.sh
+++ tests/test_ping_2022.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-LOSS=$(sudo ping -qi 0.02 -s 1400 -c 1000 $(ip r show default | sort -nk7 | awk '{print $3; exit}') | awk -F, '/packet loss/ {print $3}' | sed 's=%.*==g' | sed 's= ==')
+LOSS=$(sudo ping -qi 0.02 -s 1400 -c 1000 $(ip r show default | sort -nk7 | grep eth0 | awk '{print $3; exit}') | awk -F, '/packet loss/ {print $3}' | sed 's=%.*==g' | sed 's= ==')
+
+LOSS=${LOSS:="-1"}
 
 EID=$(redis-cli hget sys:ept eid)
 

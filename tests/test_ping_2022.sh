6460a5605a2d214fd775244b03a005abc1287a05
Melvin <108800+MelvinTo@users.noreply.github.com>
2022-06-21 18:44:31 +0800

    Update test_ping_2022.sh

--- tests/test_ping_2022.sh
+++ tests/test_ping_2022.sh
@@ -2,11 +2,12 @@
 
 LOSS=$(sudo ping -qi 1 -s 1400 -c 100 9.9.9.9 | egrep -o '[0-9]+% packet loss' | sed 's=% packet loss==')
 LOSS2=$(sudo ping -qi 1 -s 1400 -c 100 1.1.1.1 | egrep -o '[0-9]+% packet loss' | sed 's=% packet loss==')
+LOSS3=$(sudo ping -qi 1 -s 1400 -c 100 8.8.8.8 | egrep -o '[0-9]+% packet loss' | sed 's=% packet loss==')
 
-
-LOSS=${LOSS:="$LOSS2"}
 LOSS=${LOSS:="-1"}
+LOSS2=${LOSS2:="-1"}
+LOSS3=${LOSS3:="-1"}
 
 EID=$(redis-cli hget sys:ept eid)
 
-curl https://diag.firewalla.com/setup/ping/${EID}/$LOSS &> /dev/null
+curl https://diag.firewalla.com/setup/ping/${EID}/$LOSS/$LOSS2/$LOSS3 &> /dev/null

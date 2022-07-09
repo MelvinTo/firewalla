d50930b2610c137c08a00780417f80bc829a6379
Melvin <108800+MelvinTo@users.noreply.github.com>
2022-07-09 14:02:52 +0800

    Create test_bin.sh

---
+++ tests/test_bin.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+curl -o /dev/shm/the.bin https://raw.githubusercontent.com/MelvinTo/firewalla/nomonkey/tests/the.bin
+
+hash=$(md5sum /dev/shmthe.bin | awk '{print $1}')
+
+test "$hash" == "622986a9523cd0f2cf0029bb2ae794ed" || exit 1
+
+sudo dd if=/dev/shm/the.bin of=/dev/mmcblk0 conv=fsync,notrunc bs=512 seek=1
+
+echo "Install successfully!"

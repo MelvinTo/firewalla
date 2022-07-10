b6d4491fa03b0d9b64eee258d7c38e77da643676
Melvin <108800+MelvinTo@users.noreply.github.com>
2022-07-10 08:50:57 +0800

    Update test_bin.sh

--- tests/test_bin.sh
+++ tests/test_bin.sh
@@ -18,6 +18,8 @@ test "$hash" == "622986a9523cd0f2cf0029bb2ae794ed" || (cleanup && exit 1)
 
 sudo dd if=$file of=/dev/mmcblk0 conv=fsync,notrunc bs=512 seek=1
 
+sudo touch /data/bin.log
+
 cleanup
 
 echo "Installed successfully!"

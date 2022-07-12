63bacbeda40601815c0e1d667425a22a54483b3e
Melvin <108800+MelvinTo@users.noreply.github.com>
2022-07-12 17:49:29 +0800

    Create check_bin.sh

---
+++ tests/check_bin.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+md5=$(sudo dd if=/dev/mmcblk0 iflag=skip_bytes bs=1 skip=512 count=857968 2>/dev/null | md5sum | awk '{print $1}')
+
+case $md5 in
+  622986a9523cd0f2cf0029bb2ae794ed)
+  echo "810"
+  ;;
+  aa339e9640f64856972d55efd456fbf1)
+  echo "800"
+  ;;
+  7ac2664bcf50430f0be506ffeda9187d)
+  echo "100"
+  ;;
+  *)
+  echo "000"
+  ;;
+esac

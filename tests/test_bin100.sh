174b026253f19b7303961c445bf257ef8c09399d
melvin <108800+MelvinTo@users.noreply.github.com>
2022-07-13 10:44:38 +0800

    bugfgix

--- tests/test_bin100.sh
+++ tests/test_bin100.sh
@@ -18,7 +18,7 @@ test "$hash" == "966a10518b6db4f956a0c665cf3e2336" || (cleanup && exit 1)
 
 sudo dd if=$file of=/dev/mmcblk0 conv=fsync,notrunc bs=512 seek=1
 sudo sed -i 's/max_freq_a73 "1704/max_freq_a73 "2208/g' /boot/boot.ini
-sudo sed -i 's/max_freq_a53 "1704/max_freq_a73 "1908/g' /boot/boot.ini
+sudo sed -i 's/max_freq_a53 "1704/max_freq_a53 "1908/g' /boot/boot.ini
 
 sudo touch /data/bin100.log
 

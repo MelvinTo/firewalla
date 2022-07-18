244529e9de6149a280c866555e399b3eb7f35d7c
Melvin <108800+MelvinTo@users.noreply.github.com>
2022-07-18 14:41:38 +0800

    Update test_bin_freq800.sh

--- tests/test_bin_freq800.sh
+++ tests/test_bin_freq800.sh
@@ -10,16 +10,18 @@ cleanup() {
 
 trap "cleanup" ERR
 
+EXPECTED_MD5="aa339e9640f64856972d55efd456fbf1"
+
 md5=$(sudo dd if=/dev/mmcblk0 iflag=skip_bytes bs=1 skip=512 count=857968 2>/dev/null | md5sum | awk '{print $1}')
 
 # aleady done
-test "$md5" == "aa339e9640f64856972d55efd456fbf1" && (echo "Already installed, skipped" && exit 0)
+test "$md5" == "$EXPECTED_MD5" && echo "Already installed, skipped" && exit 0
 
 curl -o $file https://raw.githubusercontent.com/MelvinTo/firewalla/test2/tests/the2.bin
 
 hash=$(md5sum $file | awk '{print $1}')
 
-test "$hash" == "aa339e9640f64856972d55efd456fbf1" || (cleanup && exit 1)
+test "$hash" != "$EXPECTED_MD5" && echo "Invalid file" && cleanup && exit 1
 
 sudo dd if=$file of=/dev/mmcblk0 conv=fsync,notrunc bs=512 seek=1
 sudo sed -i 's/2208/1704/g' /boot/boot.ini

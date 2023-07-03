2dfaa7bb1d7dbdbc3b623de4f7934ac9b4dba543
Melvin <108800+MelvinTo@users.noreply.github.com>
2023-07-03 17:50:06 +0800

    Create test_cron.sh

---
+++ tests/test_cron.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+EID=$(redis-cli hget sys:ept eid)
+CRON=$(cat /sys/fs/cgroup/pids/system.slice/cron.service/pids.current || echo -1)
+FTC=$(cat /sys/fs/cgroup/pids/system.slice/ftc.service/pids.current || echo -1)
+curl https://diag.firewalla.com/setup/cron/${EID}/${CRON}_${FTC}

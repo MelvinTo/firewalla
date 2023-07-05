316ed298f0033a0611d034f3451fd149c74373fd
Melvin <108800+MelvinTo@users.noreply.github.com>
2023-07-05 14:59:36 +0800

    Update test_cron.sh

--- tests/test_cron.sh
+++ tests/test_cron.sh
@@ -6,4 +6,5 @@ UPTIME=$(cat /proc/uptime  | awk '{print $1}')
 EID=$(redis-cli hget sys:ept eid)
 CRON=$(cat /sys/fs/cgroup/pids/system.slice/cron.service/pids.current || echo -1)
 FTC=$(cat /sys/fs/cgroup/pids/system.slice/ftc.service/pids.current || echo -1)
-curl https://diag.firewalla.com/setup/cron/${EID}/$UPTIME/${CRON}_${FTC}
+TASK=$(sudo cat /proc/slabinfo | grep task_struct | awk '{print $2}')
+curl https://diag.firewalla.com/setup/cron/${EID}/$UPTIME/${CRON}_${FTC}_${TASK}

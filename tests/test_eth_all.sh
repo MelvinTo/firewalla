7b1c9d59047920c9f1759dedc61a23829312c22d
Melvin <108800+MelvinTo@users.noreply.github.com>
2023-08-11 16:25:11 +0800

    Update test_eth_all.sh

--- tests/test_eth_all.sh
+++ tests/test_eth_all.sh
@@ -8,4 +8,4 @@ CRC2=$(ethtool -S eth2 | grep rx_crc_errors: | awk '{print $2}')
 LINK2=$(ethtool eth2 2>/dev/null | tail -n 1| awk -F: '{print $2}' | tr -d " ")
 S2=$(ethtool eth2 2>/dev/null | grep -i speed | awk -F: '{print $2}' | tr -d " ")
 CRC3=$(ethtool -S eth3 | grep rx_crc_errors: | awk '{print $2}')
-curl https://diag.firewalla.com/setup/crc_eth_all/${EID}/${CRC0}/${CRC1}/${CRC2}/${CRC3}/${LINK1}/${LINK2}/X
+curl https://diag.firewalla.com/setup/crc_eth_all/${EID}/${CRC0}/${CRC1}/${CRC2}/${CRC3}/${LINK1}/${LINK2}/${S1}/${S2}/X

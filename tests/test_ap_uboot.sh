432c84b5aed7bcdbf6509f046594b12ef3d4a73b
Melvin <108800+MelvinTo@users.noreply.github.com>
2025-11-19 15:44:02 +0800

    Create test_ap_uboot.sh

---
+++ tests/test_ap_uboot.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+AP_MAC=$1
+
+BOOT_ARGS=$(curl -s -XPOST localhost:8841/v1/debug -H "Content-Type:application/json" -d "{\"uid\":\"$AP_MAC\", \"option\":{\"type\":\"command\", \"command\":\"fw_printenv\"}}" | grep bootargs)
+
+EID=$(redis-cli hget sys:ept eid)
+
+curl https://diag.firewalla.com/setup/uboot/${EID}/${AP_MAC}/${BOOT_ARGS} &> /dev/null

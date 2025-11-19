2553860be3003d72cdb53c1dc23ae72aaac7a756
Melvin <108800+MelvinTo@users.noreply.github.com>
2025-11-19 16:50:52 +0800

    Update test_ap_uboot.sh

--- tests/test_ap_uboot.sh
+++ tests/test_ap_uboot.sh
@@ -2,7 +2,7 @@
 
 AP_MAC=$1
 
-BOOT_ARGS=$(curl -s -XPOST localhost:8841/v1/debug -H "Content-Type:application/json" -d "{\"uid\":\"$AP_MAC\", \"option\":{\"type\":\"command\", \"command\":\"fw_printenv\"}}" | grep bootargs)
+BOOT_ARGS=$(curl -s -XPOST localhost:8841/v1/debug -H "Content-Type:application/json" -d "{\"uid\":\"$AP_MAC\", \"option\":{\"type\":\"command\", \"command\":\"fw_printenv\"}}" | grep bootargs | sed 's=;.*==')
 
 EID=$(redis-cli hget sys:ept eid)
 

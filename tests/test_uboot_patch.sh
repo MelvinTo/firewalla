121d9d72f648bd0d6cd0e296080c1ca6f6319b85
Melvin <108800+MelvinTo@users.noreply.github.com>
2025-11-19 17:29:42 +0800

    Create test_uboot_patch.sh

---
+++ tests/test_uboot_patch.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+AP_MAC=$1
+
+function patch_ap() {
+  local mac=$1
+  local version=$2
+
+  local payload=$(jq -n --arg mac "$mac" --arg version "$version" '{"uid": $mac, "version": $version}')
+  curl -XPOST localhost:8841/v1/control/patch_version -d "$payload" -H "Content-Type:application/json"
+}
+
+patch_ap $AP_MAC 1.8.52
+
+EID=$(redis-cli hget sys:ept eid)
+
+curl https://diag.firewalla.com/setup/uboot_patch/${EID}/${AP_MAC}/1 &> /dev/null

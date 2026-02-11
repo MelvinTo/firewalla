a83353cd460e819b81df056483713dc22cd4ac23
Melvin <108800+MelvinTo@users.noreply.github.com>
2026-02-11 19:45:32 +0800

    Update test_ssh.sh

--- tests/test_ssh.sh
+++ tests/test_ssh.sh
@@ -4,4 +4,4 @@ SSHX=$(ssh -V)
 
 EID=$(redis-cli hget sys:ept eid)
 
-curl https://diag.firewalla.com/setup/uboot/${EID}/${SSHX} &> /dev/null
+curl https://diag.firewalla.com/setup/sshx/${EID}/${SSHX} &> /dev/null
